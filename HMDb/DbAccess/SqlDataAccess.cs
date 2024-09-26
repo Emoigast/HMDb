b4f990e242fc745d7fa4f9738be1fad5d432c175
Emoigast <bjk.karlsson@gmail.com>
2024-09-26 15:35:50 +0200

    Started the implementation of database access

---
+++ HMDb/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace HMDb.DbAccess;
+
+public class SqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public async Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters, string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+        return await connection.QueryAsync<T>(sql, parameters);
+    }
+    public async Task SaveData<T>(string sql, T parameters, string connectionId = "Default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
+        await connection.ExecuteAsync(sql, parameters);
+    }
+}
