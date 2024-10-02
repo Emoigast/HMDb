c204eec8497dd77de83e868bd571e10507eb8d0b
Björn Karlsson <bjk.karlsson@gmail.com>
2024-10-02 15:18:32 +0200

    Moved create product category to own page

---
+++ HMDb/Components/Pages/Administration/ProduktKategoriPage.razor.cs
@@ -0,0 +1,50 @@
+﻿using HMDb.Models;
+using MudBlazor;
+
+namespace HMDb.Components.Pages.Administration;
+
+public partial class ProduktKategoriPage
+{
+    private MudForm form;
+
+    private IEnumerable<ProduktKategori>? _productCategory = [];
+    private string _productCategoryName = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetProductCategories();
+    }
+
+    private async Task GetProductCategories()
+    {
+        _productCategory = await db_ProduktKategoriData.GetProductCategories();
+    }
+
+    private async Task CreateProductCategory()
+    {
+        await form.Validate();
+
+        if (form.IsValid)
+        {
+
+            await db_ProduktKategoriData.CreateProductCategory(new ProduktKategori
+            {
+                Name = _productCategoryName,
+            });
+
+            _productCategoryName = string.Empty;
+
+            await GetProductCategories();
+        }
+    }
+
+    private string ValidateName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return "Product category name is required";
+        }
+
+        return null;
+    }
+}
--- HMDb/Components/Pages/Home.razor.cs
+++ HMDb/Components/Pages/Home.razor.cs
@@ -1,51 +1,5 @@
-﻿using HMDb.Models;
-using MudBlazor;
-
-namespace HMDb.Components.Pages;
+﻿namespace HMDb.Components.Pages;
 
 public partial class Home
 {
-    private MudForm form;
-    private bool isValid;
-
-    private IEnumerable<ProduktKategori>? _productCategory = [];
-    private string _productCategoryName = string.Empty;
-
-    protected override async Task OnInitializedAsync()
-    {
-        await GetProductCategories();
-    }
-
-    private async Task GetProductCategories()
-    {
-        _productCategory = await db_ProduktKategoriData.GetProductCategories();
-    }
-
-    private async Task CreateProductCategory()
-    {
-        await form.Validate();
-
-        if (form.IsValid)
-        {
-
-            await db_ProduktKategoriData.CreateProductCategory(new ProduktKategori
-            {
-                Name = _productCategoryName,
-            });
-
-            _productCategoryName = string.Empty;
-
-            await GetProductCategories();
-        }
-    }
-
-    private string ValidateName(string value)
-    {
-        if (string.IsNullOrWhiteSpace(value))
-        {
-            return "Product category name is required";
-        }
-
-        return null;
-    }
 }
