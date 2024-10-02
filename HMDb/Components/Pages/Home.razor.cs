04b1ec0c763b9706eef1803cf4809b2ee75dd6bf
Björn Karlsson <bjk.karlsson@gmail.com>
2024-10-02 14:37:18 +0200

    Started moving ProduktCategory from Home to it's own page

--- HMDb/Components/Pages/Home.razor.cs
+++ HMDb/Components/Pages/Home.razor.cs
@@ -1,23 +1,51 @@
 ﻿using HMDb.Models;
+using MudBlazor;
 
 namespace HMDb.Components.Pages;
 
 public partial class Home
 {
+    private MudForm form;
+    private bool isValid;
+
     private IEnumerable<ProduktKategori>? _productCategory = [];
+    private string _productCategoryName = string.Empty;
 
     protected override async Task OnInitializedAsync()
+    {
+        await GetProductCategories();
+    }
+
+    private async Task GetProductCategories()
     {
         _productCategory = await db_ProduktKategoriData.GetProductCategories();
     }
 
-    private string _productCategoryName = string.Empty;
-
-    private async void CreateProductCategory()
+    private async Task CreateProductCategory()
     {
-        await db_ProduktKategoriData.CreateProductCategory(new ProduktKategori
+        await form.Validate();
+
+        if (form.IsValid)
         {
-            Name = _productCategoryName,
-        });
+
+            await db_ProduktKategoriData.CreateProductCategory(new ProduktKategori
+            {
+                Name = _productCategoryName,
+            });
+
+            _productCategoryName = string.Empty;
+
+            await GetProductCategories();
+        }
+    }
+
+    private string ValidateName(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return "Product category name is required";
+        }
+
+        return null;
     }
 }
--- HMDb/DbAccess/SqlDataAccess.cs
+++ HMDb/DbAccess/SqlDataAccess.cs
@@ -7,15 +7,18 @@ namespace HMDb.DbAccess;
 public class SqlDataAccess : ISqlDataAccess
 {
     private readonly IConfiguration _configuration;
+
     public SqlDataAccess(IConfiguration configuration)
     {
         _configuration = configuration;
     }
+
     public async Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters, string connectionId = "Default")
     {
         using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
         return await connection.QueryAsync<T>(sql, parameters);
     }
+
     public async Task SaveData<T>(string sql, T parameters, string connectionId = "Default")
     {
         using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(connectionId));
