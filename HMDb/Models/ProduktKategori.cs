88b1aa40aed689afb1383b188d86fa76f3d9fb1a
Emoigast <bjk.karlsson@gmail.com>
2024-10-02 11:07:42 +0200

    Created ProduktData

--- HMDb/Models/ProduktKategori.cs
+++ HMDb/Models/ProduktKategori.cs
@@ -4,10 +4,13 @@ public class ProduktKategori
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public ProduktKategori ProductCategory { get; set; }
 
-    public ProduktKategori(int id, string name)
+
+    public ProduktKategori(int id, string name, ProduktKategori productCategory)
     {
         Id = id;
         Name = name;
+        ProductCategory = productCategory;
     }
 }
